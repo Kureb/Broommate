2dcaff40ecd68018990bff30536f08fe45d3bd70
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2016-04-28 16:34:07 +0300

    Skeleton for widget

---
+++ app/src/main/java/com/oulu/daussy/broommate/Helper/SimpleWidgetProvider.java
@@ -0,0 +1,44 @@
+package com.oulu.daussy.broommate.Helper;
+
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.oulu.daussy.broommate.R;
+
+import java.util.Random;
+
+/**
+ * Created by daussy on 28/04/16.
+ */
+public class SimpleWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int count = appWidgetIds.length;
+
+        for (int i = 0; i < count; i++) {
+            int widgetId = appWidgetIds[i];
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.simple_widget);
+
+            String groupName = "Group Name";
+            remoteViews.setTextViewText(R.id.groupNameWidget, groupName.toString());
+
+            Intent intent = new Intent(context, SimpleWidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.actionButtonWidget, pendingIntent);
+
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+
+}
